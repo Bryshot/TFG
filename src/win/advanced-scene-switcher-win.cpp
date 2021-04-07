23231bf11a95e978b386b948ccbe50695a82cd19
Bryshot <bryan9807@gmail.com>
2021-04-07 15:30:50 +0100

    Eliminacion de elementos residuales
    No debería de ser necesarios para el correcto funcionamiento del plugin

--- src/win/advanced-scene-switcher-win.cpp
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-#include <util/platform.h>
-#include "../headers/advanced-scene-switcher.hpp"
-#include <TlHelp32.h>
-#include <Psapi.h>
-
-///La funcionalidad de este fichero es implementar las funciones dependientes del SO, en concreto en este caso, las de Windows
-
